3e32c9dcaf81baa9bf210f48476c5cf6b105424d
王俊 <1127132348@qq.com>
2019-07-18 22:36:03 +0800

    vue-cli hello world

---
+++ Front/vue/cli/todo/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <todo-app></todo-app>  
+  </div>
+</template>
+
+<script>
+import TodoApp from './view/Todo.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TodoApp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
