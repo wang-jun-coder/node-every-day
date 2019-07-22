2346be4ca11758c1a741b39ed95a79679f8d39ce
王俊 <1127132348@qq.com>
2019-07-22 21:55:22 +0800

    sso-demo

---
+++ Project/sso/sub-system-2/routes/users.js
@@ -0,0 +1,40 @@
+const router = require('koa-router')();
+const superagent = require('superagent');
+
+router.prefix('/users');
+
+router.get('/', function (ctx, next) {
+  ctx.body = 'this is a users response!'
+});
+
+router.get('/login', function (ctx, next) {
+  const ssoUrl = 'http://localhost:3000/users/login';
+  const callback = 'http://localhost:3002/users/checkToken';
+
+  ctx.redirect(`${ssoUrl}?redirectUrl=${encodeURIComponent(callback)}`);
+});
+
+router.get('/logout', function (ctx, next) {
+    ctx.session.user = null;
+    let logoutUrl = `http://localhost:3000/users/logout?redirectUrl=${encodeURIComponent('http://localhost:3002/')}&token=${ctx.session.token}`;
+    ctx.session.token = null;
+    ctx.redirect(logoutUrl);
+});
+
+router.get('/checkToken', async ctx => {
+  const { token } = ctx.request.query;
+
+  const validatorUrl = 'http://localhost:3000/users/validator';
+  const ret = await superagent.post(validatorUrl)
+      .send({
+        token
+      });
+
+  if (ret.body && ret.body.code === 0) {
+    ctx.session.user = ret.body.data;
+    ctx.session.token = token;
+  }
+  ctx.redirect('/');
+});
+
+module.exports = router;
