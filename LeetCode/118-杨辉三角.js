3b8789c46870f61719e690921b8225580714dbda
王俊 <1127132348@qq.com>
2019-08-05 22:39:51 +0800

    leet-code

---
+++ LeetCode/118-杨辉三角.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+	const ret = [];
+
+	while(numRows) {
+		numRows--;
+		if (ret.length === 0) {
+			ret.push([1]);
+			continue;
+		}
+		let array = [1];
+		let pre = ret[ret.length-1];
+		for(let i=1; i<pre.length; i++) {
+			array.push(pre[i] + pre[i-1]);
+		}
+		array.push(1);
+		ret.push(array);
+	}
+	return ret;
+};
+
+
+
+console.log(generate(5));
